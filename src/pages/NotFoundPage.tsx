d628c3ef8e9339a0a92adacceb2650c3c931fe0f
MairwunNx <mairwunnx@gmail.com>
2026-09-08 18:56:33 +0200

    Add the 404 page

---
+++ src/pages/NotFoundPage.tsx
@@ -0,0 +1,13 @@
+import { Link } from 'react-router-dom'
+import styles from './NotFoundPage.module.less'
+
+export function NotFoundPage() {
+  return (
+    <div className={styles.wrap}>
+      <p className={styles.code}>404</p>
+      <h1>This rune leads nowhere</h1>
+      <p>The page you followed doesn't exist here.</p>
+      <Link to="/">Back to the front page</Link>
+    </div>
+  )
+}
